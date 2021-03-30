4ce720c377cbc9130014d45ce0a90f5d8f8b5941
Dennis Lypovetsky <tsssssssss@gmail.com>
2021-03-30 21:49:18 +0300

    Fix typo

--- HW1/khishchenko/user_name.py
+++ HW1/khishchenko/user_name.py
@@ -1,7 +1,7 @@
-name = input('What is your name?\n',)
-age = input('How old are you?\n',)
-city = input('Wher do you live?\n',)
+name = input('What is your name?\n')
+age = input('How old are you?\n')
+city = input('Where do you live?\n')
 
-print ('\nHello,', name, end='\n')
-print ('Your age is', age, end='\n')
-print ('You live in', city, end='\n')
+print('\nHello,', name)
+print('Your age is', age)
+print('You live in', city)
