b5f2402994a898a1cfe965dbdc29be14dcd656ce
Anna Khishchenko <ann.kh18@gmail.com>
2021-03-30 21:34:51 +0300

    added two files in new branch

---
+++ HW1/khishchenko/calculations.py
@@ -0,0 +1,8 @@
+number_a = int(input('Enter number a = ',))
+number_b = int(input('Enter number b = ',))
+
+print('\na + b = ', number_a + number_b)
+print('a - b = ', number_a - number_b)
+print('a * b = ', number_a * number_b)
+print('a / b = ', number_a / number_b)
+print('a ** b = ', number_a ** number_b)
---
+++ HW1/khishchenko/user_name.py
@@ -0,0 +1,7 @@
+name = input('What is your name?\n',)
+age = input('How old are you?\n',)
+city = input('Wher do you live?\n',)
+
+print ('\nHello,', name, end='\n')
+print ('Your age is', age, end='\n')
+print ('You live in', city, end='\n')
