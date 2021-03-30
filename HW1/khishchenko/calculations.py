d201d817988d3ab59d648ca38331214959b1f8ac
Dennis Lypovetsky <tsssssssss@gmail.com>
2021-03-30 21:44:58 +0300

    Fix calculations file

--- HW1/khishchenko/calculations.py
+++ HW1/khishchenko/calculations.py
@@ -1,5 +1,5 @@
-number_a = int(input('Enter number a = ',))
-number_b = int(input('Enter number b = ',))
+number_b = int(input('Enter number b = '))
+number_a = int(input('Enter number a = '))
 
 print('\na + b = ', number_a + number_b)
 print('a - b = ', number_a - number_b)
