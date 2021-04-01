d1df38d4d65288bc51e1fff78f9d49d2d1dab2be
Anna Khishchenko <ann.kh18@gmail.com>
2021-04-01 21:10:34 +0300

    fix problems with numeration and reworked the task 3.2_number.py

--- HW2/khishchenko/3.2_number.py
+++ HW2/khishchenko/3.2_number.py
@@ -1,12 +1,10 @@
 # Задано чотирицифрове натуральне число. 
 
-natural_number = str(1580)
-
-print("Natural number ", natural_number)
+natural_number = input("Enter natural number from 1000 to 9999: ")
 
 # Знайти добуток цифр цього числа.
-
-print("Sum of elements in number ", eval("1 + 5 + 8 + 0"))
+multiplication_natural_number = int(natural_number[0])*int(natural_number[1])*int(natural_number[2])*int(natural_number[3])
+print("Multiplication of elements in number ", multiplication_natural_number)
 
 # Записати число в реверсному порядку.
 
@@ -17,6 +15,6 @@ print("Reversed number ", reverse_natural_number)
 
 # Посортувати цифри, що входять в дане число.
 
-numbers = [1, 5, 8, 0]
-numbers.sort()
-print("Sort of number ", numbers)
+sort_natural_number = sorted(natural_number)
+sorted_natural_number = "".join(sort_natural_number)
+print("Sorted number ", sorted_natural_number)
