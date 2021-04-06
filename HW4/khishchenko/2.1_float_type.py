9e53e0c85e2eeaf8297dfac8d8285253e02a268d
Anna Khishchenko <ann.kh18@gmail.com>
2021-04-06 20:52:05 +0300

    added HW4 2.1_float_type.py

---
+++ HW4/khishchenko/2.1_float_type.py
@@ -0,0 +1,10 @@
+# Create a list that contains elements 
+# of integer type, then use the loop to 
+# change the type of these elements to a floating type. 
+# (Hint: use the built-in float () function). 
+
+
+list_of_integer_elements = [1, 4, 8, 5, 3 , 5]
+
+for i in list_of_integer_elements:
+    print(float(i))
