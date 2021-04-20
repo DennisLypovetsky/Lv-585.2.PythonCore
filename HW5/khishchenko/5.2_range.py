bee52bb519591f060914ad67c37f74a0f535380c
Anna Khishchenko <ann.kh18@gmail.com>
2021-04-20 12:37:42 +0300

    reworked 5.2_range.py

--- HW5/khishchenko/5.2_range.py
+++ HW5/khishchenko/5.2_range.py
@@ -1,3 +1,8 @@
+# Task2. In the range from 1 to 10 determine 
+# even numbers that are divisible by 2,
+# odd numbers, which are divisible by 3,
+# numbers that are not divisible by 2 and 3.
+
 Even = []
 Odd = []
 NumbersThatAreNotDivisibleBy2And3 = []
