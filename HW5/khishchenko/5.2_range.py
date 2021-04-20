e127f9eba86e2abae74d39a517b9619fd167baf9
Anna Khishchenko <ann.kh18@gmail.com>
2021-04-20 12:33:28 +0300

    git commit

--- HW5/khishchenko/5.2_range.py
+++ HW5/khishchenko/5.2_range.py
@@ -1,7 +1,15 @@
-for item in range(11):
+Even = []
+Odd = []
+NumbersThatAreNotDivisibleBy2And3 = []
+
+for item in range(1,10):
     if item % 2 == 0:
-        print("Even:", item)
-    elif item % 3 == 0:
-        print("Odd:", item)
-    else:
-        print("Not divisibel by 2 and 3:", item)
+        Even.append(item)
+    if item % 3 == 0:
+        Odd.append(item)
+    if item % 2 != 0 and  item % 3 != 0:
+        NumbersThatAreNotDivisibleBy2And3.append(item)
+
+print("Even numbers:", Even)
+print("Odd numbers:", Odd)
+print("Not divisibel by 2 and 3:", NumbersThatAreNotDivisibleBy2And3)
