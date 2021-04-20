4c5ac687fd24f413c268839a0ff4fa7b5eb827b7
Anna Khishchenko <ann.kh18@gmail.com>
2021-04-20 12:18:13 +0300

    added HW5 task 5.2_range.py

---
+++ HW5/khishchenko/5.2_range.py
@@ -0,0 +1,7 @@
+for item in range(11):
+    if item % 2 == 0:
+        print("Even:", item)
+    elif item % 3 == 0:
+        print("Odd:", item)
+    else:
+        print("Not divisibel by 2 and 3:", item)
